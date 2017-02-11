6a300c5bdeb9fbf25dc3232a90b30ac09ac719b8
Ben Blum <bblum@andrew.cmu.edu>
2017-02-11 12:30:22 -0500

    add zippy patch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+oldpwd=`pwd`
 cd /tmp
 #wget ftp://ftp.club.cc.cmu.edu/gnu/xboard/xboard-4.4.3.tar.gz
 #wget http://mirror.anl.gov/pub/gnu/xboard/xboard-4.4.3.tar.gz
@@ -7,7 +8,7 @@ cd /tmp
 wget http://ftp.gnu.org/gnu/xboard/xboard-4.5.2.tar.gz
 tar xvzf xboard-4.5.2.tar.gz
 cd xboard-4.5.2
-patch < ~/xboard-4.4.2-zippypassword2.patch
+patch < $oldpwd/xboard-4.4.2-zippypassword2.patch
 ./configure
 # fuck ccache
 export PATH="/usr/local/bin/:$PATH"
